b70b9fa8fba2a15bfb1df8060f8fbe9f7635a72b
Shai Dickman <shai.dickman@berkeley.edu>
2025-04-06 17:40:30 -0700

    removed eval code

--- drakes_protein/fmif/eval_finetune.py
+++ drakes_protein/fmif/eval_finetune.py
@@ -67,7 +67,7 @@ import biotite.structure.io as bsio
 #     gen_true_bbrmsd = pyrosetta.rosetta.core.scoring.bb_rmsd(true_pose, pose)
 #     return gen_true_bbrmsd
 
-def cal_rmsd(S_sp, S, batch, the_folding_model, pdb_path, mask_for_loss, save_path, args, item_idx, base_path):
+def gen_results(S_sp, S, batch, mask_for_loss, save_path, args, item_idx, base_path):
     with torch.no_grad():
         results_list = []
         run_name = save_path.split('/')
@@ -76,106 +76,107 @@ def cal_rmsd(S_sp, S, batch, the_folding_model, pdb_path, mask_for_loss, save_pa
         else:
             run_name = run_name[-1]
         
-        sc_output_dir_base = os.path.join(base_path, 'sc_eval', f'{args.decoding}_{args.base_model}_{args.dps_scale}_{args.tds_alpha}_{args.seed}_{run_name}', 'sc_output', batch["protein_name"][0][:-4])
-        sc_output_dir = os.path.join(sc_output_dir_base, 'true')
-        the_pdb_path = os.path.join(pdb_path, batch['WT_name'][0])
-        # fold the ground truth sequence
-        os.makedirs(os.path.join(sc_output_dir, 'true_seqs'), exist_ok=True)
-        true_fasta = fasta.FastaFile()
-        true_detok_seq = "".join([ALPHABET[x] for _ix, x in enumerate(S[0]) if mask_for_loss[0][_ix] == 1])
-        true_fasta['true_seq_1'] = true_detok_seq
-        true_fasta_path = os.path.join(sc_output_dir, 'true_seqs', 'true.fa')
-        true_fasta.write(true_fasta_path)
-        true_folded_dir = os.path.join(sc_output_dir, 'true_folded')
-        if os.path.exists(true_folded_dir):
-            shutil.rmtree(true_folded_dir)
-        os.makedirs(true_folded_dir, exist_ok=False)
-        true_folded_output = the_folding_model.fold_fasta(true_fasta_path, true_folded_dir)
-        true_folded_pdb_path = os.path.join(true_folded_dir, 'folded_true_seq_1.pdb')
-        true_folded_pose = pyrosetta.pose_from_file(true_folded_pdb_path)
+        # sc_output_dir_base = os.path.join(base_path, 'sc_eval', f'{args.decoding}_{args.base_model}_{args.dps_scale}_{args.tds_alpha}_{args.seed}_{run_name}', 'sc_output', batch["protein_name"][0][:-4])
+        # sc_output_dir = os.path.join(sc_output_dir_base, 'true')
+        # the_pdb_path = os.path.join(pdb_path, batch['WT_name'][0])
+        # # fold the ground truth sequence
+        # os.makedirs(os.path.join(sc_output_dir, 'true_seqs'), exist_ok=True)
+        # true_fasta = fasta.FastaFile()
+        # true_detok_seq = "".join([ALPHABET[x] for _ix, x in enumerate(S[0]) if mask_for_loss[0][_ix] == 1])
+        # true_fasta['true_seq_1'] = true_detok_seq
+        # true_fasta_path = os.path.join(sc_output_dir, 'true_seqs', 'true.fa')
+        # true_fasta.write(true_fasta_path)
+        # true_folded_dir = os.path.join(sc_output_dir, 'true_folded')
+        # if os.path.exists(true_folded_dir):
+        #     shutil.rmtree(true_folded_dir)
+        # os.makedirs(true_folded_dir, exist_ok=False)
+        # true_folded_output = the_folding_model.fold_fasta(true_fasta_path, true_folded_dir)
+        # true_folded_pdb_path = os.path.join(true_folded_dir, 'folded_true_seq_1.pdb')
+        # true_folded_pose = pyrosetta.pose_from_file(true_folded_pdb_path)
 
-        print("    Computing True Folded Relax...")
-        scorefxn = pyrosetta.create_score_function("ref2015_cart")
-        tf = TaskFactory()
-        tf.push_back(RestrictToRepacking())
-        packer = PackRotamersMover(scorefxn, tf.create_task_and_apply_taskoperations(true_folded_pose))
-        packer.apply(true_folded_pose)
-        relax = FastRelax()
-        relax.set_scorefxn(scorefxn)
-        relax.apply(true_folded_pose)
-        true_folded_relax_path = os.path.join(sc_output_dir, 'true_folded_relax', 'folded_true_relax_seq_1.pdb')
-        os.makedirs(os.path.join(sc_output_dir, 'true_folded_relax'), exist_ok=True)
-        true_folded_pose.dump_pdb(true_folded_relax_path)
+        # print("    Computing True Folded Relax...")
+        # scorefxn = pyrosetta.create_score_function("ref2015_cart")
+        # tf = TaskFactory()
+        # tf.push_back(RestrictToRepacking())
+        # packer = PackRotamersMover(scorefxn, tf.create_task_and_apply_taskoperations(true_folded_pose))
+        # packer.apply(true_folded_pose)
+        # relax = FastRelax()
+        # relax.set_scorefxn(scorefxn)
+        # relax.apply(true_folded_pose)
+        # true_folded_relax_path = os.path.join(sc_output_dir, 'true_folded_relax', 'folded_true_relax_seq_1.pdb')
+        # os.makedirs(os.path.join(sc_output_dir, 'true_folded_relax'), exist_ok=True)
+        # true_folded_pose.dump_pdb(true_folded_relax_path)
 
-        true_pose = pyrosetta.pose_from_file(the_pdb_path)
-        scorefxn = pyrosetta.create_score_function("ref2015_cart")
-        tf = TaskFactory()
-        tf.push_back(RestrictToRepacking())
-        packer = PackRotamersMover(scorefxn, tf.create_task_and_apply_taskoperations(true_pose))
-        packer.apply(true_pose)
-        relax = FastRelax()
-        relax.set_scorefxn(scorefxn)
-        relax.apply(true_pose)
+        # true_pose = pyrosetta.pose_from_file(the_pdb_path)
+        # scorefxn = pyrosetta.create_score_function("ref2015_cart")
+        # tf = TaskFactory()
+        # tf.push_back(RestrictToRepacking())
+        # packer = PackRotamersMover(scorefxn, tf.create_task_and_apply_taskoperations(true_pose))
+        # packer.apply(true_pose)
+        # relax = FastRelax()
+        # relax.set_scorefxn(scorefxn)
+        # relax.apply(true_pose)
 
-        os.makedirs(os.path.join(sc_output_dir, 'true_relax'), exist_ok=True)
-        true_pose.dump_pdb(os.path.join(sc_output_dir, 'true_relax', 'true_relax_seq_1.pdb'))
+        # os.makedirs(os.path.join(sc_output_dir, 'true_relax'), exist_ok=True)
+        # true_pose.dump_pdb(os.path.join(sc_output_dir, 'true_relax', 'true_relax_seq_1.pdb'))
 
-        foldtrue_true_bbrmsd = pyrosetta.rosetta.core.scoring.bb_rmsd(true_pose, true_folded_pose)
+        # foldtrue_true_bbrmsd = pyrosetta.rosetta.core.scoring.bb_rmsd(true_pose, true_folded_pose)
     
-        print("    Computing Samples Folded Relax...")
+        # print("    Computing Samples Folded Relax...")
+        true_detok_seq = "".join([ALPHABET[x] for _ix, x in enumerate(S[0]) if mask_for_loss[0][_ix] == 1])
+        
         print(f"     - Memory: {torch.cuda.mem_get_info()}")
         for _it, ssp in tqdm(enumerate(S_sp)):
             num = item_idx * 16 + _it
-            sc_output_dir = os.path.join(sc_output_dir_base, f'{num}')
-            os.makedirs(sc_output_dir, exist_ok=True)
-            os.makedirs(os.path.join(sc_output_dir, 'fmif_seqs'), exist_ok=True)
-            codesign_fasta = fasta.FastaFile()
-            detok_seq = "".join([ALPHABET[x] for _ix, x in enumerate(ssp) if mask_for_loss[_it][_ix] == 1])
-            codesign_fasta['codesign_seq_1'] = detok_seq
-            codesign_fasta_path = os.path.join(sc_output_dir, 'fmif_seqs', 'codesign.fa')
-            codesign_fasta.write(codesign_fasta_path)
+            # sc_output_dir = os.path.join(sc_output_dir_base, f'{num}')
+            # os.makedirs(sc_output_dir, exist_ok=True)
+            # os.makedirs(os.path.join(sc_output_dir, 'fmif_seqs'), exist_ok=True)
+            # codesign_fasta = fasta.FastaFile()
+            # detok_seq = "".join([ALPHABET[x] for _ix, x in enumerate(ssp) if mask_for_loss[_it][_ix] == 1])
+            # codesign_fasta['codesign_seq_1'] = detok_seq
+            # codesign_fasta_path = os.path.join(sc_output_dir, 'fmif_seqs', 'codesign.fa')
+            # codesign_fasta.write(codesign_fasta_path)
 
-            folded_dir = os.path.join(sc_output_dir, 'folded')
-            if os.path.exists(folded_dir):
-                shutil.rmtree(folded_dir)
-            os.makedirs(folded_dir, exist_ok=False)
+            # folded_dir = os.path.join(sc_output_dir, 'folded')
+            # if os.path.exists(folded_dir):
+            #     shutil.rmtree(folded_dir)
+            # os.makedirs(folded_dir, exist_ok=False)
 
-            folded_output = the_folding_model.fold_fasta(codesign_fasta_path, folded_dir)
-            gen_folded_pdb_path = os.path.join(folded_dir, 'folded_codesign_seq_1.pdb')
-            pose = pyrosetta.pose_from_file(gen_folded_pdb_path)
-            scorefxn = pyrosetta.create_score_function("ref2015_cart")
-            tf = TaskFactory()
-            tf.push_back(RestrictToRepacking())
-            packer = PackRotamersMover(scorefxn, tf.create_task_and_apply_taskoperations(pose))
-            packer.apply(pose)
-            relax = FastRelax()
-            relax.set_scorefxn(scorefxn)
-            relax.apply(pose)
+            # folded_output = the_folding_model.fold_fasta(codesign_fasta_path, folded_dir)
+            # gen_folded_pdb_path = os.path.join(folded_dir, 'folded_codesign_seq_1.pdb')
+            # pose = pyrosetta.pose_from_file(gen_folded_pdb_path)
+            # scorefxn = pyrosetta.create_score_function("ref2015_cart")
+            # tf = TaskFactory()
+            # tf.push_back(RestrictToRepacking())
+            # packer = PackRotamersMover(scorefxn, tf.create_task_and_apply_taskoperations(pose))
+            # packer.apply(pose)
+            # relax = FastRelax()
+            # relax.set_scorefxn(scorefxn)
+            # relax.apply(pose)
 
-            os.makedirs(os.path.join(sc_output_dir, 'folded_relax'), exist_ok=True)
-            pose.dump_pdb(os.path.join(sc_output_dir, 'folded_relax', 'folded_codesign_relax_seq_1.pdb'))
+            # os.makedirs(os.path.join(sc_output_dir, 'folded_relax'), exist_ok=True)
+            # pose.dump_pdb(os.path.join(sc_output_dir, 'folded_relax', 'folded_codesign_relax_seq_1.pdb'))
 
-            gen_true_bbrmsd = pyrosetta.rosetta.core.scoring.bb_rmsd(true_pose, pose)
-            gen_foldtrue_bbrmsd = pyrosetta.rosetta.core.scoring.bb_rmsd(true_folded_pose, pose)
+            # gen_true_bbrmsd = pyrosetta.rosetta.core.scoring.bb_rmsd(true_pose, pose)
+            # gen_foldtrue_bbrmsd = pyrosetta.rosetta.core.scoring.bb_rmsd(true_folded_pose, pose)
             seq_revovery = (S_sp[_it] == S[0]).float().mean().item()
-
-            resultdf = pd.DataFrame(columns=['gen_true_bb_rmsd', 'gen_foldtrue_bb_rmsd', 'foldtrue_true_bb_rmsd', 'seq_recovery'])
-            resultdf.loc[0] = [gen_true_bbrmsd, gen_foldtrue_bbrmsd, foldtrue_true_bbrmsd, seq_revovery]
+            resultdf = pd.DataFrame(columns=['seq_recovery'])
+            resultdf.loc[0] = [seq_revovery]
             resultdf['seq'] = "".join([ALPHABET[x] for _ix, x in enumerate(ssp) if mask_for_loss[_it][_ix] == 1])
             resultdf['true_seq'] = true_detok_seq
             resultdf['protein_name'] = batch['protein_name'][0]
             resultdf['WT_name'] = batch['WT_name'][0]
             resultdf['num'] = num
-            resultdf['pdbpath'] = sc_output_dir
+            # resultdf['pdbpath'] = sc_output_dir
             results_list.append(resultdf)
 
     return results_list
 
 
-def parse_df(results_df):
-    avg_rmsd = results_df['bb_rmsd'].mean()
-    success_rate = results_df['bb_rmsd'].apply(lambda x: 1 if x < 2 else 0).mean()
-    return avg_rmsd, success_rate, np.format_float_positional(avg_rmsd, unique=False, precision=3), np.format_float_positional(success_rate, unique=False, precision=3)
+# def parse_df(results_df):
+#     # avg_rmsd = results_df['bb_rmsd'].mean()
+#     success_rate = results_df['bb_rmsd'].apply(lambda x: 1 if x < 2 else 0).mean()
+#     return success_rate#avg_rmsd, success_rate, np.format_float_positional(avg_rmsd, unique=False, precision=3), np.format_float_positional(success_rate, unique=False, precision=3)
 
 
 argparser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -462,15 +463,15 @@ for n in [10]:
                                 total_prop += sum(mask_detect) / len(mask_detect)
                             mask_proportion[i] += total_prop / len(S_sp_traj)
                         total_seq_count += 1 # Terrible code, here for clarity :)
-                    dg_pred = reward_model(X, S_sp, mask, chain_M, residue_idx, chain_encoding_all)
-                    rewards.append(dg_pred.detach().cpu().numpy())
-                    dg_pred_eval = reward_model_eval(X, S_sp, mask, chain_M, residue_idx, chain_encoding_all)
-                    rewards_eval.append(dg_pred_eval.detach().cpu().numpy())
+                    # dg_pred = reward_model(X, S_sp, mask, chain_M, residue_idx, chain_encoding_all)
+                    # rewards.append(dg_pred.detach().cpu().numpy())
+                    # dg_pred_eval = reward_model_eval(X, S_sp, mask, chain_M, residue_idx, chain_encoding_all)
+                    # rewards_eval.append(dg_pred_eval.detach().cpu().numpy())
                     true_false_sp = (S_sp == S).float()
                     mask_for_loss = mask*chain_M
                     valid_sp_acc += torch.sum(true_false_sp * mask_for_loss).cpu().data.numpy()
                     valid_sp_weights += torch.sum(mask_for_loss).cpu().data.numpy()
-                    results_list = cal_rmsd(S_sp, S, batch, the_folding_model, pdb_path, mask_for_loss, save_path, args, item_idx, args.base_path)
+                    results_list = gen_results(S_sp, S, batch, mask_for_loss, save_path, args, item_idx, args.base_path)
                     results_merge.extend(results_list)
                     break
                 break
@@ -484,41 +485,41 @@ for n in [10]:
                 writer.writerows(data)
             print(mask_proportion)
             print(reward_average)
-            continue
+            # continue
             valid_sp_accuracy = valid_sp_acc / valid_sp_weights
             print('Sequence recovery accuracy: ', valid_sp_accuracy)
 
-            rewards_eval = np.hstack(rewards_eval)
-            rewards = np.hstack(rewards)
-            print('Mean reward: ', rewards_eval.mean(), "Positive reward prop %f"%np.mean(rewards_eval>0), "Mean reward (ft): ", rewards.mean(), "Positive reward prop (ft) %f"%np.mean(rewards>0))
-            print('median reward: ', np.median(rewards_eval), 'median reward (ft): ', np.median(rewards))
+            # rewards_eval = np.hstack(rewards_eval)
+            # rewards = np.hstack(rewards)
+            # print('Mean reward: ', rewards_eval.mean(), "Positive reward prop %f"%np.mean(rewards_eval>0), "Mean reward (ft): ", rewards.mean(), "Positive reward prop (ft) %f"%np.mean(rewards>0))
+            # print('median reward: ', np.median(rewards_eval), 'median reward (ft): ', np.median(rewards))
 
             results_merge = pd.concat(results_merge)
-            avg_rmsd = results_merge['gen_true_bb_rmsd'].mean()
-            mid_rmsd = results_merge['gen_true_bb_rmsd'].median()
-            rmsd_rate = results_merge['gen_true_bb_rmsd'].apply(lambda x: 1 if x < 2 else 0).mean()
-            print('Median gen_true RMSD: ', mid_rmsd, 'Mean gen_true RMSD: ', avg_rmsd, 'Good RMSD prop: ', rmsd_rate)
+            # avg_rmsd = results_merge['gen_true_bb_rmsd'].mean()
+            # mid_rmsd = results_merge['gen_true_bb_rmsd'].median()
+            # rmsd_rate = results_merge['gen_true_bb_rmsd'].apply(lambda x: 1 if x < 2 else 0).mean()
+            # print('Median gen_true RMSD: ', mid_rmsd, 'Mean gen_true RMSD: ', avg_rmsd, 'Good RMSD prop: ', rmsd_rate)
 
-            results_merge['rewards_eval'] = rewards_eval
-            results_merge['rewards'] = rewards
-            results_merge['success'] = (results_merge['gen_true_bb_rmsd'] < 2) & (results_merge['rewards_eval'] > 0)
+            # results_merge['rewards_eval'] = rewards_eval
+            # results_merge['rewards'] = rewards
+            # results_merge['success'] = (results_merge['gen_true_bb_rmsd'] < 2) & (results_merge['rewards_eval'] > 0)
 
-            success_rate = results_merge['success'].mean()
-            print('success rate: ', success_rate)
+            # success_rate = results_merge['success'].mean()
+            # print('success rate: ', success_rate)
 
-            results_merge.to_csv(f'./eval_results/{args.decoding}_{args.base_model}_{args.dps_scale}_{args.tds_alpha}_{args.seed}_results_merge_{test_name}.csv')
+            results_merge.to_csv(f'./eval_results/{args.decoding}_{args.base_model}_{args.dps_scale}_{args.tds_alpha}_{args.seed}_results_merge_{test_name}.csv', index=False)
 
-            results_dict = {'Sequence Recovery Accuracy': valid_sp_accuracy, 
-                            #'Model Log Likelihood': all_model_logl.mean(), 
-                            'Mean Reward': rewards_eval.mean(), 
-                            'Positive Reward Proportion': np.mean(rewards_eval>0), 
-                            'Mean Reward (ft)': rewards.mean(), 
-                            'Positive Reward Proportion (ft)': np.mean(rewards>0), 
-                            'Median Reward': np.median(rewards_eval), 
-                            'Median Reward (ft)': np.median(rewards), 
-                            'Median gen_true RMSD': mid_rmsd, 
-                            'Mean gen_true RMSD': avg_rmsd,
-                            'Good RMSD Proportion': rmsd_rate,
-                            'Success Rate': success_rate} 
-            results_df_final = pd.DataFrame.from_dict(results_dict, orient='index', columns=['Value'])
-            results_df_final.to_csv(f'./eval_results/{args.decoding}_{args.base_model}_{args.dps_scale}_{args.tds_alpha}_{args.seed}_results_summary_{test_name}.csv')
+            # results_dict = {'Sequence Recovery Accuracy': valid_sp_accuracy, 
+            #                 #'Model Log Likelihood': all_model_logl.mean(), 
+            #                 'Mean Reward': rewards_eval.mean(), 
+            #                 'Positive Reward Proportion': np.mean(rewards_eval>0), 
+            #                 'Mean Reward (ft)': rewards.mean(), 
+            #                 'Positive Reward Proportion (ft)': np.mean(rewards>0), 
+            #                 'Median Reward': np.median(rewards_eval), 
+            #                 'Median Reward (ft)': np.median(rewards), 
+            #                 'Median gen_true RMSD': mid_rmsd, 
+            #                 'Mean gen_true RMSD': avg_rmsd,
+            #                 'Good RMSD Proportion': rmsd_rate,
+            #                 'Success Rate': success_rate} 
+            # results_df_final = pd.DataFrame.from_dict(results_dict, orient='index', columns=['Value'])
+            # results_df_final.to_csv(f'./eval_results/{args.decoding}_{args.base_model}_{args.dps_scale}_{args.tds_alpha}_{args.seed}_results_summary_{test_name}.csv')
