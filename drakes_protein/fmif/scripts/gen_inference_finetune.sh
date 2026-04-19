e0cfcfe13f8158608ac12b8fa9dbc121ecb904a3
Shai Dickman <shai.dickman@berkeley.edu>
2026-04-19 00:26:03 -0400

    udpated gen scripts

--- drakes_protein/fmif/scripts/gen_inference_finetune.sh
+++ drakes_protein/fmif/scripts/gen_inference_finetune.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-#SBATCH --partition=gpu
+#SBATCH --partition=ai
+#SBATCH --time=12:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=16G
 #SBATCH --job-name=protein
 #SBATCH --output=worker_%j.out
-
-# *********************************************************************************#
-# *** Uncomment the below if not running with ./scripts/batch_gen_inference.sh *** #
-# *********************************************************************************#
-# NUM_WORKERS=1
-# WORKER_ID=0
-# *********************************************************************************#
 
 if [ -z "$WORKER_ID" ]; then
   WORKER_ID=0
@@ -22,24 +16,25 @@ fi
 echo "Running worker with ID: $WORKER_ID"
 echo "Number of workers: $NUM_WORKERS"
 
-BASE_PATH="/home/shai/BLISS_Experiments/DRAKES/DRAKES/data/data_and_model"
+BASE_PATH="/anvil/scratch/x-sdickman/data/data_and_model"
 BATCH_REPEAT=1
-BATCH_SIZE=1
+BATCH_SIZE=15
 MODEL="pretrained"
-DATASET="single"
+DATASET="validation"
 ALIGN_TYPE='bon'
-ALIGN_N=10
+ALIGN_N=1
 ORACLE_MODE='ddg'
-LASSO_LAMBDA=0.0
-SPEC_FEEDBACK_ITS=5
-FEEDBACK_METHOD="exclusion"
+LASSO_LAMBDA=0.1
+SPEC_FEEDBACK_ITS=3
+FEEDBACK_METHOD="lasso"
 MAX_SPEC_ORDER=10 # [2, 5, 10, 20]
-NUM_SPEC_MASKS=8192
-REWARD_BATCH_MAX=True
-SEED=14
+NUM_SPEC_MASKS=2048
+REWARD_BATCH_MAX=False
+SEED=0
+GBT_ARGS='{"num_leaves": 50, "learning_rate": 0.01, "max_depth": 5, "lambda_l1": 0.0001}'
 # BEAM_W=1
 # STEPS_PER_LEVEL=1
-TARGET_PROTEIN="2KRU"
+# TARGET_PROTEIN="2KRU"
 # ORACLE_ALPHA=1.0
 # MH_TYPE="uniform"
 # MH_N=0
@@ -52,19 +47,24 @@ else
     REWARD_BATCH_MAX_STR=""
 fi
 
-OUTPUT_FOLDER="/home/shai/BLISS_Experiments/DRAKES/DRAKES/drakes_protein/fmif/eval_results/test"
+OUTPUT_FOLDER="/anvil/scratch/x-sdickman/DRAKES/drakes_protein/fmif/eval_results"
 
-source /opt/miniconda/etc/profile.d/conda.sh
+#source /opt/miniconda/etc/profile.d/conda.sh
 
-if [ "$ORACLE_MODE" = 'scrmsd' ]; then
-        echo "Activating multiflow conda environment"
-        conda activate multiflow
-        echo "Set to:"$CONDA_PREFIX
-else
-        echo "Activating mf2 conda environment"
-        conda activate mf2
-        echo "Set to:"$CONDA_PREFIX
-fi
+#if [ "$ORACLE_MODE" = 'scrmsd' ]; then
+#        echo "Activating multiflow conda environment"
+#        conda activate multiflow
+#        echo "Set to:"$CONDA_PREFIX
+#else
+#        echo "Activating mf2 conda environment"
+#        conda activate mf2
+#        echo "Set to:"$CONDA_PREFIX
+#fi
+
+module load conda
+module load gurobi
+
+conda activate mf2
 
 python gen_inference_finetune.py --base_path=$BASE_PATH \
         --batch_repeat=$BATCH_REPEAT \
@@ -84,5 +84,6 @@ python gen_inference_finetune.py --base_path=$BASE_PATH \
         --feedback_method=$FEEDBACK_METHOD \
         $REWARD_BATCH_MAX_STR \
         --num_spec_masks=$NUM_SPEC_MASKS \
-        --lasso_lambda=$LASSO_LAMBDA \
-        --target_protein=$TARGET_PROTEIN
+        --gbt_args="$GBT_ARGS" \
+        --lasso_lambda=$LASSO_LAMBDA #\
+        # --target_protein=$TARGET_PROTEIN
